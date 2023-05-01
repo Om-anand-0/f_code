797ef1ceb0bed30383396090fe3d273e88282d12
Om Anand <82884162+Om-anand-0@users.noreply.github.com>
2023-05-01 13:28:38 -0700

    Update pass_or_fail.c
    added else condition

--- code/pass_or_fail.c
+++ code/pass_or_fail.c
@@ -12,21 +12,38 @@ int main()
     scanf("%f", &c);
     printf("Enter your Physics marks : \n");
     scanf("%f", &p);
+
     if (m > 33)
     {
         printf("Congratulations You've passed in Maths \n");
+    }
+    else
+    {
+        printf("Sorry You've failed in Maths \n");
     }
     if (c > 33)
     {
         printf("Congratulations You've passed in Chemistry \n");
     }
+    else
+    {
+        printf("Sorry You've failed in Chemistry \n");
+    }
     if (p > 33)
     {
         printf("Congratulations You've passed in Physics \n");
     }
+    else
+    {
+        printf("Sorry You've failed in Physics \n");
+    }
     if (m + c + p / z * 100 > 40)
     {
-        printf("Congratulations You've passed in total \n");
+        printf("Congratulations You've passed in all subjects \n");
+    }
+    else
+    {
+        printf("Sorry You've failed in all subjects \n");
     }
 
     return 0;
