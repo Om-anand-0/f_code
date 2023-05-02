9d47e8bffe73e2da3d8874fd60601556ae1e17e4
Om Anand <82884162+Om-anand-0@users.noreply.github.com>
2023-05-02 13:07:39 -0700

    Update pass_or_fail.c
    added Percentage , Fiexed some bugs.

--- code/pass_or_fail.c
+++ code/pass_or_fail.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    float m, c, p, z;
+    float m, c, p, z, y;
     printf("welcome to pass or fail calculator \n");
     printf("Enter Maximum marks in one subject : \n");
     scanf("%f", &z);
@@ -13,7 +13,7 @@ int main()
     printf("Enter your Physics marks : \n");
     scanf("%f", &p);
 
-    if (m > 33)
+    if (m / z * 100 >= 33)
     {
         printf("Congratulations You've passed in Maths \n");
     }
@@ -21,7 +21,7 @@ int main()
     {
         printf("Sorry You've failed in Maths \n");
     }
-    if (c > 33)
+    if (c / z * 100 >= 33)
     {
         printf("Congratulations You've passed in Chemistry \n");
     }
@@ -29,7 +29,7 @@ int main()
     {
         printf("Sorry You've failed in Chemistry \n");
     }
-    if (p > 33)
+    if (p / z * 100 >= 33)
     {
         printf("Congratulations You've passed in Physics \n");
     }
@@ -37,14 +37,17 @@ int main()
     {
         printf("Sorry You've failed in Physics \n");
     }
-    if (m + c + p / z * 100 > 40)
+    if ((m + c + p) / (3*z) * 100 > 40)
     {
-        printf("Congratulations You've passed in all subjects \n");
+        printf("Congratulations You've passed in Total \n");
     }
     else
     {
-        printf("Sorry You've failed in all subjects \n");
+        printf("Sorry You've failed in Total \n");
     }
+        y = (m + c + p) / (3*z) * 100;
+
+        printf("Your total percentage is %f \n", y );
 
     return 0;
 }
