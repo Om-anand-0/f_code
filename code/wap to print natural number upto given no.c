fe66284ce730e921af145882940d50682622e53e
Om Anand <82884162+Om-anand-0@users.noreply.github.com>
2023-11-25 13:53:30 +0530

    Create wap to print natural number upto given no.c

---
+++ code/wap to print natural number upto given no.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+        int i,n;
+        printf("Enter the Number\n");
+        scanf("%d", &n);
+        for( i=1; i<=n; i++){
+                printf("%d \n", i);
+        }
+}
+
